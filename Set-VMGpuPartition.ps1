412ab5b490e0c5c1b098ee9cdf86b389da9c02f8
Werner <guilherme.werner@outlook.com>
2023-04-22 13:20:49 -0300

    Update Set-VMGpuPartition.ps1

--- Set-VMGpuPartition.ps1
+++ Set-VMGpuPartition.ps1
@@ -1,8 +1,16 @@
-$vm = "Win10"
+$VMName = "Win11"
 
-Add-VMGpuPartitionAdapter -VMName $vm
-Set-VMGpuPartitionAdapter -VMName $vm -MinPartitionVRAM 80000000 -MaxPartitionVRAM 100000000 -OptimalPartitionVRAM 100000000 -MinPartitionEncode 80000000 -MaxPartitionEncode 100000000 -OptimalPartitionEncode 100000000 -MinPartitionDecode 80000000 -MaxPartitionDecode 100000000 -OptimalPartitionDecode 100000000 -MinPartitionCompute 80000000 -MaxPartitionCompute 100000000 -OptimalPartitionCompute 100000000
+Set-VMProcessor -VMName $VMName -ExposeVirtualizationExtensions $true
 
-Set-VM -GuestControlledCacheTypes $true -VMName $vm
-Set-VM -LowMemoryMappedIoSpace 1Gb -VMName $vm
-Set-VM –HighMemoryMappedIoSpace 32GB –VMName $vm
+Add-VMGpuPartitionAdapter -VMName $VMName
+Set-VMGpuPartitionAdapter -VMName $VMName -MinPartitionVRAM 80000000 -MaxPartitionVRAM 100000000 -OptimalPartitionVRAM 100000000 -MinPartitionEncode 80000000 -MaxPartitionEncode 100000000 -OptimalPartitionEncode 100000000 -MinPartitionDecode 80000000 -MaxPartitionDecode 100000000 -OptimalPartitionDecode 100000000 -MinPartitionCompute 80000000 -MaxPartitionCompute 100000000 -OptimalPartitionCompute 100000000
+
+Set-VM -GuestControlledCacheTypes $true -VMName $VMName
+Set-VM -LowMemoryMappedIoSpace 1Gb -VMName $VMName
+Set-VM –HighMemoryMappedIoSpace 32GB –VMName $VMName
+
+$VMObj = Get-VM "WinSrv22"
+Remove-VMGpuPartitionAdapter -VM $VMObj
+
+
+
